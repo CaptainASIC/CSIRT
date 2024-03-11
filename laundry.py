a253f079b04679fb41b5f6b13f0bb2796410d304
Samuel Presgraves <samuel.presgraves@lixil.com>
2024-03-11 12:43:55 +0900

    bugfix

--- laundry.py
+++ laundry.py
@@ -1,18 +1,23 @@
 import sys
 import os
+from pathlib import Path
 
-# Function to check for graphical environment
+# Modify the Python path to include the 'lib' directory
+script_dir = Path(__file__).resolve().parent
+lib_dir = script_dir / 'lib'
+sys.path.append(str(lib_dir))
+
+# Import the GUI or CLI module from the 'lib' directory
+from main import MainApp as GUIApp
+from lndcli import main as cli_main
+
 def is_graphical_environment():
     return os.environ.get('DISPLAY', None) is not None or os.environ.get('WAYLAND_DISPLAY', None) is not None
 
 if __name__ == "__main__":
-    # Check for '--cli' argument
-    cli_mode = '--cli' in sys.argv
-
-    # Decide whether to run in CLI mode or GUI mode
-    if cli_mode or not is_graphical_environment():
-        from lib import lndcli as cli_functions
-        cli_functions.main()
+    # Decide whether to run in CLI mode or GUI mode based on environment and arguments
+    if '--cli' in sys.argv or not is_graphical_environment():
+        cli_main()
     else:
-        from lib import main as gui_application
-        gui_application.root.mainloop()
+        app = GUIApp()
+        app.mainloop()
